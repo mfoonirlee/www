c09b23e83d9bf06567f45363e9ee3d7b883031b2
hong.li <hong.li@Ctrip.com>
2015-12-23 16:45:19 +0800

    add all files and finish act`s pages

---
+++ dedemao/js/exp.js
@@ -0,0 +1,94 @@
+$(function(){
+    /*
+    * @description: 体验活动列表逻辑
+    * @auther: mf...
+    */
+
+    var hotTplFun = _.template($('#j_hotact_tpl').html()),
+        actTplFun = _.template($('#j_act_tpl').html()),
+        CONST_WIDTH = 371 + 24 + 2,
+        deltax = 0,
+        //显示元素的总长度
+        totalWidth = 0,
+        CONST_CONTAINER_WIDTH = 1200 - 12;
+
+    $.ajax({
+        type: 'GET',
+        url: Config.getUrl('explist'),
+        contentType: 'application/json;charset=utf-8',
+        dataType: 'json',
+        success: function (data) {
+            Config.log(data);
+            data = data.concat(data).concat(data).concat(data).concat(data);
+            ajaxCallBack(data);
+        },
+        error: function () {
+
+        }
+    });
+
+    function ajaxCallBack(data){
+        //控制容器展示
+        if(!data){
+            return;
+        }
+
+        totalWidth = CONST_WIDTH * data.length;
+
+        var newActivityList = _.groupBy(data, function(n){return n.isnew != '1'})['true'];
+        if(newActivityList){
+            newActivityList = newActivityList.slice(0, 6);
+        }else{
+            newActivityList = [];
+        }
+
+        $('#j_hot_container').html(hotTplFun({data: newActivityList})).children().on('click', function(e){
+            onClickItem(e);
+        });
+        $('#j_act_container').html(actTplFun({data: data})).children().on('click', function(e){
+            onClickItem(e);
+        });
+
+        $('#j_left').on('click', function(){
+            if(deltax < 0){
+                deltax += CONST_CONTAINER_WIDTH;
+            }
+            // else{
+            //     deltax = 0;
+            // }
+            turnPage(deltax);
+        });
+
+        $('#j_right').on('click', function(){
+            if(Math.abs(deltax) < totalWidth - CONST_CONTAINER_WIDTH){
+                deltax -= CONST_CONTAINER_WIDTH;
+            }
+            // else{
+            //     deltax = 0;
+            // }
+            turnPage(deltax);
+        });
+    }
+
+    //点击了某个item
+    function onClickItem (e) {
+        var $target = $(e.target || e.srcElement);
+        if($target[0].tagName != 'li'){
+            $target = $target.parents('li');
+        }
+
+        var id = $target.data('id');
+        location.href = Config.getPageURL('expdetial') + '&aid=' + id;
+    }
+
+    function turnPage () {
+        $('#j_act_container').animate({left: deltax});
+    }
+
+    //在调试模式下，将内置函数外露
+    if(Config.isDebug){
+        window.ajaxCallBack = ajaxCallBack;
+        window.onClickItem = onClickItem;
+    }
+
+}());
---
+++ dedemao/js/util.js
@@ -0,0 +1,36 @@
+$(function () {
+    var Util = {
+        getQueryParam: function (key) {
+            var param = {},
+                search = window.location.search;
+                search = search && search.replace('?', '').split('&') || [];
+            for (var i = 0, length = search.length; i < length; i++) {
+                var q = search[i].split('=');
+                param[q[0]] = q[1];
+            }
+            return key ? (param[key] || '') : param;
+        },
+        replaceUrlParam: function(str, val){
+            return str && str.replace(/{\w+}/, val);
+        },
+
+        parseTimeStr: function(timeStr, form){
+            var d = new Date(timeStr),
+                dateObj = {
+                            year: d.getFullYear(),
+                            month: (d.getMonth() + 1),
+                            date: d.getDate(),
+                            hour: d.getHours(),
+                            min: d.getMinutes()
+                        };
+            form = form.replace(/y/, dateObj.year);
+            form = form.replace(/m/, dateObj.month);
+            form = form.replace(/d/, dateObj.date);
+            form = form.replace(/H/, dateObj.hour);
+            form = form.replace(/i/, dateObj.min);
+            return form;
+        }
+    };
+
+    window.Util = Util;
+}());
