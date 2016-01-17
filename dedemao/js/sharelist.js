0aaf4bc3210e1c38a58024997adb0dfc5d7b038b
hong.li <hong.li@Ctrip.com>
2016-01-17 19:45:34 +0800

    fix spell bug

--- dedemao/js/sharelist.js
+++ dedemao/js/sharelist.js
@@ -64,7 +64,7 @@
         });
         //for detail page
         $('#j_plist_container').delegate('.j_detail', 'click', function(e){
-            var $target = $(e.currentTarget).parnets('li'),
+            var $target = $(e.currentTarget).parents('li'),
                 id = $target.data('id');
 
             forwardDetail(id);
