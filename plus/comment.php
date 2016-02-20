e8f50a80767841285179e0623d7c19bfa89a3bf8
nanhuacrab <nanhuacrab@hotmail.com>
2016-02-20 11:04:26 +0800

    君禾社区/评论/报名

--- plus/comment.php
+++ plus/comment.php
@@ -5,7 +5,17 @@ ini_set('date.timezone', 'Asia/Shanghai');
 
 session_start();
 $aid = $_GET['aid'];
-$info=$_POST['info'];
+$postdata = file_get_contents("php://input");
+$info= substr($postdata,5);
+
+$info = urldecode($info);
+
+$infoType = mb_detect_encoding($info, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $infoType != 'GBK')
+{
+  $info = mb_convert_encoding($info ,'GBK' , $infoType);
+}
+
 $uid = $_SESSION['_id'];
 $uname = $_SESSION['_name'];
 $ualias=$_SESSION['_alias'];
@@ -19,25 +29,41 @@ $query = "INSERT INTO `dede_feedback`(`aid`,`mid`,`username`,`alias`,`avatar`,`d
 	'".$uname."', '".$ualias."', '".$uavatar."',".$time.",'".$info."',".$lve."); ";
  $num= $dsql->ExecuteNoneQuery($query);
 $result = array();
-if($num<1)
-{
-$result["IsSuccess"]=0;
-$result["msg"]="Comment Failed!";
-}
-else
+if($num)
 {
 $result["IsSuccess"]=1;
-$result["msg"]="Comment Successfully!";
+$data="评论成功";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
+}
+else
+{
+$result["IsSuccess"]=0;
+$data="评论失败";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
 }
 }
 else
 {
 $result["IsSuccess"]=0;
-$result["msg"]="Comment Failed!";
+$data="尚未登陆,登陆页面跳转中...";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
 $result["url"]="http://112.124.110.58:8081/user.php";
 }
 
 echo json_encode($result);
-
-
 ?>
--- plus/signup.php
+++ plus/signup.php
@@ -5,30 +5,70 @@ session_start();
 $aid = $_GET['aid'];
 $uid = $_SESSION['_id'];
 $uname = $_SESSION['_name'];
+$unameType = mb_detect_encoding($uname, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $unameType != 'GBK')
+{
+  $uname = mb_convert_encoding($uname ,'GBK' , $unameType);
+}
 $ualias=$_SESSION['_alias'];
 $uavatar = $_SESSION['_img'];
 
 if (isset($uid))
 {
+$sql = "select Count(1)as signupnum from dede_activitysignup WHERE activityid=".$_GET["aid"]." and userid=".$uid.";";
+$row = $dsql->GetOne($sql);
+
+if($row['signupnum']>=1)
+{
+$result["IsSuccess"]=0;
+$data="该活动您已经报名，不可重复报名";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
+}
+else
+{
 $query = "INSERT INTO `dede_activitysignup`(`activityid`,`userid`,`user_name`,`alias`,`avatar`) VALUES (".$aid.", ".$uid.", 
 	'".$uname."', '".$ualias."', '".$uavatar."'); ";
 $num= $dsql->ExecuteNoneQuery($query);
 $result = array();
-if($num<1)
-{
-$result["IsSuccess"]=0;
-$result["msg"]="Sign Up Failed!";
-}
-else
+if($num)
 {
 $result["IsSuccess"]=1;
-$result["msg"]="Sign Up Successfully!";
+$data="报名成功";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
+}
+else
+{
+$result["IsSuccess"]=0;
+$data="报名失败";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
+}
 }
 }
 else
 {
 $result["IsSuccess"]=0;
-$result["msg"]="Sign Up Failed!";
+$data="尚未登陆,登陆页面跳转中...";
+$fileType = mb_detect_encoding($data, array('UTF-8','GBK','LATIN1','BIG5')) ;
+if( $fileType != 'UTF-8')
+{
+  $data = mb_convert_encoding($data ,'utf-8' , $fileType);
+}
+$result["msg"]=$data;
 $result["url"]="http://112.124.110.58:8081/user.php";
 }
 echo json_encode($result);
