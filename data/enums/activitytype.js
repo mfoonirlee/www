13584806c3f8ab28c665a9ca18a412a3caa90502
nanhuacrab <nanhuacrab@hotmail.com>
2016-03-12 21:39:47 +0800

    活动类型数据库

---
+++ data/enums/activitytype.js
@@ -0,0 +1,11 @@
+<!--
+em_activitytypes=new Array();
+em_activitytypes[500]='休闲养生';
+em_activitytypes[1000]='花草种植';
+em_activitytypes[1500]='手工坊';
+em_activitytypes[2000]='孩子成长';
+em_activitytypes[2500]='农耕日';
+em_activitytypes[3000]='24节气';
+em_activitytypes[3500]='琴棋书画';
+em_activitytypes[4000]='定制主题日';
+-->
